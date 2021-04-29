a5925f0f70ae2e0642554c0f3fd0596f25e544ad
Arturo Vial Arqueros <tukovial@hotmail.com>
2021-04-28 22:44:15 -0400

    Added Step Card

---
+++ recipe-app/src/app/step-card/step-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step-card',
+  templateUrl: './step-card.component.html',
+  styleUrls: ['./step-card.component.scss'],
+})
+export class StepCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
